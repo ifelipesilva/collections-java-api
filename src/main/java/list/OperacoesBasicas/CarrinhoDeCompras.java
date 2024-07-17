865e66a889bd7d30a57d149b3b869086d545f5bc
ifelipesilva <ifelipesilvapro@gmail.com>
2024-07-17 17:01:00 -0300

    Resolvendo desafio listaTarefa
    Signed-off-by: ifelipesilva <ifelipesilvapro@gmail.com>

---
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,16 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+        private List<Item> itemList;
+
+
+        public CarrinhoDeCompras(){
+            this.itemList = new ArrayList<>();
+        }
+
+
+}
